78251e134cc98d3078561f1c3c69512cbe413abc
kariolos <l.g.karolos@gmail.com>
2017-02-16 17:11:35 +0200

    final Commit

--- src/Bookkeeper/ManagerBundle/Controller/BookController.php
+++ src/Bookkeeper/ManagerBundle/Controller/BookController.php
@@ -12,12 +12,34 @@ class BookController extends Controller
 {	
 	#Dispally all item
 	public function indexAction(){
-		return $this->render('BookkeeperManagerBundle:Book:index.html.twig');
+		#Instance of entity manager
+		$em = $this->getDoctrine()->getManager();
+		#Create new object containing all items
+		$books = $em->getRepository('BookkeeperManagerBundle:Book')->findAll();
+		#Rendex indx page passing books object
+		return $this->render('BookkeeperManagerBundle:Book:index.html.twig',array(
+			'books' => $books
+			));
 	}
 	
 	#Shows one item
 	public function showAction($id){
-		return $this->render('BookkeeperManagerBundle:Book:show.html.twig');	
+		#Instance of entity manager
+		$em = $this->getDoctrine()->getManager();
+		#Create new object for the specific id 
+		$book = $em->getRepository('BookkeeperManagerBundle:Book')->find($id);
+		#Create a delete form
+		$delete_form = $this->createFormBuilder()
+							->setAction($this->generateUrl('book_delete',array('id' => $id)))
+							->setMethod('DELETE')
+							->add('submit', SubmitType::class, array('label'=>'Delete Book'))
+							->getForm();
+
+		#Rendex indx page passing book object
+		return $this->render('BookkeeperManagerBundle:Book:show.html.twig',array(
+			'book' => $book,
+			'delete_form' => $delete_form->createView()
+			));	
 	}
 	
 	#Dispalays a form for creating a new item
@@ -59,8 +81,8 @@ class BookController extends Controller
 			$em->flush();
 			#Success message
 			$this->get('session')->getFlashBag()->add('msg','Your Book has been created!');
-			#reirect
-			return $this->redirect($this->generateUrl('book_new'));
+			#redirect
+			return $this->redirect($this->generateUrl('book_show',array('id'=> $book->getId())));
 		}
 		#Flash message if it didnt passed the validation
 		$this->get('session')->getFlashBag()->add('msg','Something went WRONG!');
@@ -72,15 +94,75 @@ class BookController extends Controller
 
 	#Displays a form for editing a new item
 	public function editAction($id){
-		return $this->render('BookkeeperManagerBundle:Book:edit.html.twig');
+		#Instance of entity manager
+		$em = $this->getDoctrine()->getManager();
+		#Create new object for the specific id 
+		$book = $em->getRepository('BookkeeperManagerBundle:Book')->find($id);
+		#Create Form object
+		$form = $this->createForm(BookType::class, $book, array(
+			'action' => $this->generateUrl('book_update',array('id' => $book->getId())),
+			'method' => 'PUT'
+			));
+		#Add submit Button
+		$form->add('submit', SubmitType::class, array('label'=>'Update Book'));	
+		#Render edit page passing the form object with createView property
+		return $this->render('BookkeeperManagerBundle:Book:edit.html.twig',array(
+			'form' => $form->createView()
+			));
 	}
 	#Handles form submission for editing
 	public function updateAction(Request $request, $id){
-		
+		#Instance of entity manager
+		$em = $this->getDoctrine()->getManager();
+		#Create new object for the specific id 
+		$book = $em->getRepository('BookkeeperManagerBundle:Book')->find($id);
+		#Create Form object
+		$form = $this->createForm(BookType::class, $book, array(
+			'action' => $this->generateUrl('book_update',array('id'=> $book->getId())),
+			'method' => 'PUT'
+			));
+		#Add submit Button
+		$form->add('submit', SubmitType::class, array('label'=>'Update Book'));	
+		#Handle the request
+		$form->handleRequest($request);
+		#Check validation
+		if($form->isValid()){
+			#Save
+			$em->flush();
+			#Flash message if it didnt passed the validation
+			$this->get('session')->getFlashBag()->add('msg','Your Book has been Updated!');
+			#Redirect
+			return $this->redirect($this->generateUrl('book_show',array('id'=>$id))); 
+		}	
+
+		return $this->render('BookkeeperManagerBundle:Book:edit.html.twig',array(
+			'form' => $form->createView
+			));
 	}
 
 	#Deletes an item
 	public function deleteAction(Request $request, $id){
+		#Create a delete form
+		$delete_form = $this->createFormBuilder()
+							->setAction($this->generateUrl('book_delete',array('id' => $id)))
+							->setMethod('DELETE')
+							->add('submit', SubmitType::class, array('label'=>'Delete Book'))
+							->getForm();
+		$delete_form->handleRequest($request);
 		
+		if($delete_form->isValid()){
+			#Instance of entity manager
+			$em = $this->getDoctrine()->getManager();
+			#Create new object for the specific id 
+			$book = $em->getRepository('BookkeeperManagerBundle:Book')->find($id);
+			#Remove
+			$em->remove($book);
+			#Save the action
+			$em->flush();
+		}
+			#Flash message
+			$this->get('session')->getFlashBag()->add('msg','Your Book has been Deleted!');
+			#Redirect
+			return $this->redirect($this->generateUrl('book')); 
 	}
 }
